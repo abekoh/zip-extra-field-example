0764654d8fe53a914bbf4d7aa7e4def485fe9f00
abekoh <abekoh.bcky@gmail.com>
2022-10-15 11:05:07 +0900

    feat: add main.go

---
+++ main.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"archive/zip"
+	"log"
+	"os"
+	"time"
+)
+
+var JST *time.Location
+
+func init() {
+	os.Setenv("TZ", "UTC")
+	jst, err := time.LoadLocation("Asia/Tokyo")
+	if err != nil {
+		log.Fatal(err)
+	}
+	JST = jst
+}
+
+func main() {
+	saveAsZip("/tmp/default.zip", "default.txt", "default", false)
+	saveAsZip("/tmp/in_jst.zip", "in_jst.txt", "in_jst", true)
+}
+
+func saveAsZip(path, txtFileName, txtContent string, inJST bool) {
+	f, err := os.Create(path)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f.Close()
+
+	zipWriter := zip.NewWriter(f)
+	defer zipWriter.Close()
+
+	modified := func() time.Time {
+		if inJST {
+			return time.Now()
+		} else {
+			return time.Now().In(JST)
+		}
+	}()
+	zipFile, err := zipWriter.CreateHeader(&zip.FileHeader{
+		Name:     txtFileName,
+		Modified: modified,
+	})
+	_, err = zipFile.Write([]byte(txtContent))
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = zipWriter.Close()
+	if err != nil {
+		log.Fatal(err)
+	}
+}
